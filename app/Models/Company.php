7a223ca3e1c95d803a7e02cb36d9b2e32d87dd07
Salix Nibigira <Salix>
2026-03-09 05:39:43 +0100

    Add Company model fillable fields

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'enterprise_number',
+        'vat_number',
+        'name',
+        'status',
+        'legal_form',
+        'street',
+        'postal_code',
+        'city',
+        'start_date',
+    ];
 }
